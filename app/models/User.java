d32fb49218feadde7eae71b2cb351d2a4b3f42c3
muamerlaptop <muamerpolic@Muamers-MacBook-Pro.local>
2017-06-16 10:35:47 +0200

    Models + Database connection
    Summary: Added the required models
    
    Test Plan: Plan
    
    Reviewers: asmir.avdicevic
    
    Differential Revision: http://phab.symphony.is:80/D11

---
+++ app/models/User.java
@@ -0,0 +1,59 @@
+package models;
+
+import java.util.*;
+import javax.persistence.*;
+import javax.validation.Constraint;
+
+import com.avaje.ebean.Model;
+import play.data.format.*;
+import play.data.validation.*;
+import sun.misc.Contended;
+import com.avaje.ebean.annotation.JsonIgnore;
+
+
+/**
+ * Created by muamerpolic on 6/15/17.
+ */
+@Entity
+public class User extends Model{
+
+    @Id
+    public Long Id;
+
+    @Constraints.Required
+    @Constraints.Email
+    public String emailAdress;
+    public String getEmailAdress(){
+        return emailAdress;
+    }
+    public void setEmailAddress(String emailAddress) {
+        this.emailAdress = emailAddress;
+    }
+
+    @Constraints.Required
+    @Constraints.MinLength(6)
+    public String password;
+
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    @Constraints.Required
+    public String fullName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    @JsonIgnore
+    public List<Ticket> tickets = new ArrayList<Ticket>();
+
+
+    public User(String emailAddress, String password, String fullName) {
+        setEmailAddress(emailAddress);
+        setPassword(password);
+        this.fullName = fullName;
+    }
+
+    public static Finder<Long, User> find = new Finder<Long,User>(User.class);
+}
